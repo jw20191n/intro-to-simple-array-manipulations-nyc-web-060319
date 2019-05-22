2ebc95bd0c85665a41d2396257e54316bc9a666f
Jiayi Wang <clairejw1015@gmail.com>
2019-05-22 22:10:00 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,39 @@
+def using_push(array,string)
+  array.push(string)
+  return array
+end
+
+
+def using_unshift(array, string)
+  array.unshift(string)
+  return array
+end
+
+
+def using_pop(array)
+  array.pop()
+end
+
+
+def pop_with_args(array)
+  new_array = [ ]
+  new_array.unshift(array.pop())
+  new_array.unshift(array.pop())
+  return new_array
+end
+
+
+def using_shift(array)
+  array.shift()
+end
+
+def shift_with_args(array)
+  new_array = []
+  new_array.push(array.shift())
+  new_array.push(array.shift())
+  return new_array
+end
+
+def using_concat(array1,array2)
+  array1.concat(array2)
+end
